dd3957a516a7bcdc928dfeb83dd198fd2823c595
thewayofyomi <sean.humphreys808@outlook.com>
2023-09-28 18:18:08 -0400

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
