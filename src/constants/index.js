7bdade70f96a698572174374fbf21095d9750c95
thewayofyomi <sean.humphreys808@outlook.com>
2023-09-29 18:07:32 -0400

    completed about sectionWrapper components

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,11 +42,11 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Front End Developer",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Web Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Applications Developer",
       icon: creator,
     },
   ];
