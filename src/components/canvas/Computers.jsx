17e95562484af45caf144c47346b163e2e7a24e8
Sean Humphreys <130619362+thewayofyomi@users.noreply.github.com>
2023-11-25 03:14:06 -0500

    updated Computer component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,15 +7,15 @@ const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    <mesh>
+    <group>
       <hemisphereLight intensity={3} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={2}
-        castShadow
         shadow-mapSize={1024}
+        castShadow
       />
       <pointLight intensity={2} />
       <primitive
@@ -24,7 +24,7 @@ const Computers = ({ isMobile }) => {
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
-    </mesh>
+    </group>
   );
 };
 
@@ -32,21 +32,16 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the "isMobile" state variable
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted/s
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -55,11 +50,10 @@ const ComputersCanvas = () => {
   return (
     <Canvas
       frameloop="demand"
-      shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
-      className="sm:block"
+      shadows
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
